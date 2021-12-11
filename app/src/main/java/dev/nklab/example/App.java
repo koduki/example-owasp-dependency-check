324d189362c0c46b3ba9e0b56e9b23e86aa9b4a3
koduki <koduki@nklab.dev>
2021-12-11 05:11:26 +0000

    Init Project

---
+++ app/src/main/java/dev/nklab/example/App.java
@@ -0,0 +1,12 @@
+package dev.nklab.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
