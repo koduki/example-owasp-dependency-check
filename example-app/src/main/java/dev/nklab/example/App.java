8c3a0eec00b9e97383d1169ba169772f2861440b
koduki <koduki@nklab.dev>
2021-12-11 08:35:11 +0000

    Modify for test

--- example-app/src/main/java/dev/nklab/example/App.java
+++ example-app/src/main/java/dev/nklab/example/App.java
@@ -1,12 +1,7 @@
 package dev.nklab.example;
 
-import org.apache.logging.log4j.LogManager;
-
 public class App {
     public static void main(String[] args) {
-        var logger = LogManager.getLogger();
-        
-        logger.error("Hello Log4j2");
         MyLib.log("Hello Dependency");
     }
 }
